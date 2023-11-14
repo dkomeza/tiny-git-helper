9aec9a7718c2da7251d29bb759bc91200b69c523
Dawid Komęza <dawid.komeza@gmail.com>
2023-11-14 15:47:36 +0000

    :sparkles: Add help page

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 use std::env;
 
+fn print_help() {
+    println!("Usage:");
+    println!("    commands:");
+    println!("        commit: commit changes");
+    println!("        clone: clone a repository into a new directory");
+    println!("        init: create an empty GitHub repository");
+    println!("        settings: view and edit settings");
+}
+
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
 
@@ -12,8 +21,10 @@ fn main() {
             "clone" => println!("Cloning..."),
             "init" => println!("Initializing..."),
             "settings" => println!("Settings..."),
+            "help" => print_help(),
             _ => {
                 println!("Unknown command: {}", mode);
+                print_help();
             }
         }
     }
