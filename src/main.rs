fa44b49a17e01f6cfb9d94f2215b9756085ad682
Dawid Komęza <dawid.komeza@gmail.com>
2023-11-14 15:44:54 +0000

    :sparkles: Parse basic CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() == 0 {
+    } else {
+        let mode = args[0].clone();
+
+        match mode.as_str() {
+            "commit" => println!("Committing..."),
+            "clone" => println!("Cloning..."),
+            "init" => println!("Initializing..."),
+            "settings" => println!("Settings..."),
+            _ => {
+                println!("Unknown command: {}", mode);
+            }
+        }
+    }
 }
