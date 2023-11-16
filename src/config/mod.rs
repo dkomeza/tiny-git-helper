5a3a3f00de980a8e3bdfaa6fab75cf823b86fdaa
Dawid Komęza <dawid.komeza@gmail.com>
2023-11-16 17:15:39 +0000

    :sparkles: Get home path

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,5 +1,15 @@
-pub struct Config {}
+pub struct Config {
+}
 
 pub fn load_config() -> Config {
     Config {}
+}
+
+fn get_config_path() -> String {
+    use home::home_dir;
+
+    let home = home_dir().unwrap();
+    let config_path = format!("{}/.config/tgh/config.json", home.display());
+
+    return config_path;
 }
