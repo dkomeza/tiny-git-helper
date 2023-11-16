0aeff8ec4a6b2cf3f7385a53309be667b87bb4f3
Dawid Komęza <dawid.komeza@gmail.com>
2023-11-16 17:51:59 +0000

    :sparkles: Add text input

---
+++ src/utils/input.rs
@@ -0,0 +1,11 @@
+pub fn text_input(message: &str) -> String {
+    use std::io::{stdin, stdout, Write};
+
+    let mut input = String::new();
+
+    print!("{}", message);
+    let _ = stdout().flush();
+    stdin().read_line(&mut input).expect("Failed to read input");
+
+    return input.trim().to_string();
+}
