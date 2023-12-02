81899655cb9476198d9082a2a9a28dfce67f0152
Dawid Komęza <dawid.komeza@gmail.com>
2023-12-02 14:23:28 +0000

    :sparkles: Add password input

--- src/utils/input.rs
+++ src/utils/input.rs
@@ -15,3 +15,24 @@ pub fn text(message: &str, required: bool) -> String {
 
     return input.trim().to_string();
 }
+
+pub fn password(message: &str, required: bool) -> String {
+    use crate::out;
+    use std::io::{stdin, stdout, Write};
+
+    let mut input = String::new();
+
+    print!("{}\x1B[8m", message);
+
+    let _ = stdout().flush();
+    stdin().read_line(&mut input).expect("Failed to read input");
+
+    print!("\x1B[28m");
+
+    if required && input.trim().len() == 0 {
+        out::print_error("This field is required.\n");
+        return password(message, required);
+    }
+
+    return input.trim().to_string();
+}
