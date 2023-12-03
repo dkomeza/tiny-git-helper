c9992032d634946c0062e4120b226020aec1ae2a
Dawid Komęza <dawid.komeza@gmail.com>
2023-12-03 09:02:51 +0000

    :sparkles: Add confirm input

--- src/utils/input.rs
+++ src/utils/input.rs
@@ -36,3 +36,27 @@ pub fn password(message: &str, required: bool) -> String {
 
     return input.trim().to_string();
 }
+
+pub fn confirm(message: &str, default: bool) -> bool {
+    use crate::out;
+    use std::io::{stdin, stdout, Write};
+
+    let mut input = String::new();
+
+    print!("{} [{}]: ", message, if default { "Y/n" } else { "y/N" });
+    let _ = stdout().flush();
+    stdin().read_line(&mut input).expect("Failed to read input");
+
+    if input.trim().len() == 0 {
+        return default;
+    }
+
+    return match input.trim().to_lowercase().as_str() {
+        "y" | "yes" => true,
+        "n" | "no" => false,
+        _ => {
+            out::print_error("Invalid input.\n");
+            confirm(message, default)
+        }
+    };
+}
