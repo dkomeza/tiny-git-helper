24c2fb8dfbc4fd2da9ca9ee45408f218d2512d76
Dawid Komęza <dawid.komeza@gmail.com>
2023-12-07 21:10:59 +0100

    :sparkles: Check if there are files to commit before commit menu

--- src/views/commit.rs
+++ src/views/commit.rs
@@ -1,12 +1,17 @@
 pub fn commit_menu() {
-    use crate::clear_screen;
+    use crate::{clear_screen, functions};
     use inquire::Select;
     use std::process;
 
     clear_screen();
 
-    if !crate::functions::is_git_repo() {
+    if !functions::is_git_repo() {
         crate::out::print_error("Not a git repository");
+        process::exit(0);
+    }
+
+    if !functions::commit::are_files_to_commit() {
+        crate::out::print_error("No files to commit");
         process::exit(0);
     }
 
