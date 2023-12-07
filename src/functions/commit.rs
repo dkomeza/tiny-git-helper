4edd2832593f1d7b35bbffd4a60ad6daaa4a6c52
Dawid Komęza <dawid.komeza@gmail.com>
2023-12-07 21:22:16 +0100

    :sparkles: Add is_valid_commit function

--- src/functions/commit.rs
+++ src/functions/commit.rs
@@ -1,4 +1,18 @@
-pub fn are_files_to_commit() -> bool {
+pub fn is_valid_commit() {
+    use std::process;
+
+    if !super::is_git_repo() {
+        crate::out::print_error("Not a git repository");
+        process::exit(0);
+    }
+
+    if !are_files_to_commit() {
+        crate::out::print_error("No files to commit");
+        process::exit(0);
+    }
+}
+
+fn are_files_to_commit() -> bool {
     use std::process::Command;
 
     let output = Command::new("git")
