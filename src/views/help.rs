b472773983d07764554cd2ae48e9c38d48e3ef53
Dawid Komęza <dawid.komeza@gmail.com>
2023-12-07 20:50:46 +0100

    :hammer: Edit help message content

--- src/views/help.rs
+++ src/views/help.rs
@@ -17,12 +17,13 @@ pub fn print_help(error: bool, args: crate::Args) {
     println!("");
     println!("Usage:");
     println!("  commands:");
-    println!("      commit: commit changes");
-    println!("      clone: clone a repository into a new directory");
-    println!("      init: create an empty GitHub repository");
+    println!("      commit: commit changes (WIP)");
+    println!("      clone: clone a repository into a new directory (WIP)");
+    println!("      init: create an empty GitHub repository (WIP)");
     println!("      login: login to GitHub");
-    println!("      settings: view and edit settings");
+    println!("      settings: view and edit settings (WIP)");
     println!("      version: show the version of tgh");
+    println!("      help: show this help message");
     println!("  short commands:");
     println!("      c: commit changes");
     println!("      ca: commit all changes");
