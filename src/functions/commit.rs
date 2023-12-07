3e105e660bf1e19c1cc13b1f50c23a3fbd0da52d
Dawid Komęza <dawid.komeza@gmail.com>
2023-12-07 21:18:20 +0100

    :sparkles: Check if tgh is running in the top directory
    Check if tgh was run in the top directory of the git repository

--- src/functions/commit.rs
+++ src/functions/commit.rs
@@ -13,3 +13,27 @@ pub fn are_files_to_commit() -> bool {
 
     true
 }
+
+pub fn is_top_level() -> bool {
+    use std::process::Command;
+
+    let output = Command::new("git")
+        .arg("rev-parse")
+        .arg("--show-toplevel")
+        .output()
+        .unwrap();
+
+    if !output.status.success() {
+        return false;
+    }
+
+    let out = String::from_utf8(output.stdout).unwrap();
+    let out = out.trim();
+    let current_dir = std::env::current_dir().unwrap();
+
+    if out != current_dir.to_str().unwrap() {
+        return false;
+    }
+
+    true
+}
