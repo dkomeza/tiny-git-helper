e89a47ced101fdabf5669bba1f2b0c7eaa9110b6
Dawid Komęza <dawid.komeza@gmail.com>
2023-12-10 20:58:30 +0100

    🔨 Move git related config into a separate file

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -4,6 +4,7 @@ use serde::{Deserialize, Serialize};
 
 pub mod defines;
 pub mod utils;
+mod git;
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Config {
@@ -22,7 +23,7 @@ pub struct Config {
 /// * `args` - A vector of the command line arguments.
 pub async fn check_prerequisites() {
     // Check if git is installed
-    if !check_git() {
+    if !git::check_git() {
         out::print_error("Error: Git is not installed.\n");
         println!("Please install using the link below:");
         println!("\x1B[mhttps://git-scm.com/downloads\x1B[m\n");
@@ -30,7 +31,7 @@ pub async fn check_prerequisites() {
     }
 
     // Check for git config
-    check_git_config();
+    git::check_git_config();
 
     // Check for a config file
     if !utils::config_exists() {
@@ -49,96 +50,7 @@ pub async fn check_prerequisites() {
         std::thread::sleep(std::time::Duration::from_secs(2));
     }
 }
-fn check_git() -> bool {
-    let mut command = std::process::Command::new("git");
-    command.arg("--version");
 
-    let output = command.output().unwrap();
-
-    if output.status.success() {
-        return true;
-    }
-
-    return false;
-}
-/// Checks if the user has a git config. (user.name, user.email)
-fn check_git_config() {
-    let mut command = std::process::Command::new("git");
-    command.args(["config", "--global", "user.name"]);
-
-    let output = command.output().unwrap();
-
-    if !output.status.success() {
-        out::print_error("Error: user.name was not found in git config.\n");
-        let name = ask_git_name();
-
-        let mut command = std::process::Command::new("git");
-        command.args(["config", "--global", "user.name", &name]);
-
-        let output = command.output().unwrap();
-
-        if !output.status.success() {
-            out::print_error("Error: Failed to set user.name.\n");
-            std::process::exit(1);
-        }
-    }
-
-    let mut stdout = String::from_utf8(output.stdout).unwrap();
-    stdout = stdout.trim().to_string();
-
-    if stdout.len() == 0 {
-        out::print_error("Error: user.name was not found in git config.\n");
-        let name = ask_git_name();
-
-        let mut command = std::process::Command::new("git");
-        command.args(["config", "--global", "user.name", &name]);
-
-        let output = command.output().unwrap();
-
-        if !output.status.success() {
-            out::print_error("Error: Failed to set user.name.\n");
-            std::process::exit(1);
-        }
-    }
-
-    let mut command = std::process::Command::new("git");
-    command.args(["config", "--global", "user.email"]);
-
-    let output = command.output().unwrap();
-
-    if !output.status.success() {
-        out::print_error("Error: user.email was not found in git config.\n");
-        let email = ask_git_email();
-
-        let mut command = std::process::Command::new("git");
-        command.args(["config", "--global", "user.email", &email]);
-
-        let output = command.output().unwrap();
-
-        if !output.status.success() {
-            out::print_error("Error: Failed to set user.email.\n");
-            std::process::exit(1);
-        }
-    }
-
-    let mut stdout = String::from_utf8(output.stdout).unwrap();
-    stdout = stdout.trim().to_string();
-
-    if stdout.len() == 0 {
-        out::print_error("Error: user.email was not found in git config.\n");
-        let email = ask_git_email();
-
-        let mut command = std::process::Command::new("git");
-        command.args(["config", "--global", "user.email", &email]);
-
-        let output = command.output().unwrap();
-
-        if !output.status.success() {
-            out::print_error("Error: Failed to set user.email.\n");
-            std::process::exit(1);
-        }
-    }
-}
 fn check_token() -> bool {
     if !utils::config_exists() || !utils::validate_config_file() {
         return false;
@@ -437,20 +349,7 @@ fn ask_fancy() -> bool {
         }
     }
 }
-fn ask_git_name() -> String {
-    let name = inquire::Text::new("Enter name used for git:")
-        .with_validator(inquire::required!("Name is required."))
-        .prompt();
 
-    return name.unwrap();
-}
-fn ask_git_email() -> String {
-    let email = inquire::Text::new("Enter email used for git:")
-        .with_validator(utils::validate_email)
-        .prompt();
-
-    return email.unwrap();
-}
 
 fn update_token(token: String) {
     let config = utils::read_config();
