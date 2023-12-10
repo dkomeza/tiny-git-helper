eea62df7e144da6a10869947d8e8cb6825c5c748
Dawid Komęza <dawid.komeza@gmail.com>
2023-12-10 18:45:35 +0100

    🚑 Fix the install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,13 @@ APP_NAME="tgh-$OS-$ARCH-$VERSION"
 REPO="dkomeza/tiny-git-helper"
 INSTALL_DIR="/usr/local/bin"
 
+check_sudo() {
+    if [ "$(id -u)" -ne 0 ]; then
+        echo "Please run as root."
+        exit 1
+    fi
+}
+
 get_latest_version() {
     VERSION=$(curl -s https://api.github.com/repos/$REPO/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
     if [ -z "$VERSION" ]; then
@@ -38,20 +45,20 @@ update_app_name() {
 
 download_and_install() {
     DOWNLOAD_URL="https://github.com/$REPO/releases/download/$VERSION/$APP_NAME"
+    HTTP_STATUS_CODE=$(curl -s -L -o /dev/null -w "%{http_code}" "$DOWNLOAD_URL")
 
-    echo "Downloading $DOWNLOAD_URL"
-    # HTTP_STATUS_CODE=$(curl -s -L -o /dev/null -w "%{http_code}" "$DOWNLOAD_URL")
-    # if [ "$HTTP_STATUS_CODE" -eq 200 ]; then
-    #     curl -L --output tgh "$DOWNLOAD_URL"
-    #     chmod +x tgh
-    #     mv tgh "$INSTALL_DIR"
-    #     echo "Installation complete."
-    # else
-    #     echo "No suitable version found for your system."
-    #     exit 1
-    # fi
+    if [ "$HTTP_STATUS_CODE" -eq 200 ]; then
+        curl -L --output tgh "$DOWNLOAD_URL"
+        chmod +x tgh
+        mv tgh "$INSTALL_DIR"
+        echo "Installation complete."
+    else
+        echo "No suitable version found for your system."
+        exit 1
+    fi
 }
 
+check_sudo
 check_supported_os
 check_supported_arch
 get_latest_version
