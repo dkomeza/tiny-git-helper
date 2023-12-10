b630b44904b7159db655092978b345420cc3c233
Dawid Komęza <dawid.komeza@gmail.com>
2023-12-10 20:19:44 +0100

    ✨ Update check_git_config function
    Check if name or email is empty

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -83,12 +83,48 @@ fn check_git_config() {
         }
     }
 
+    let mut stdout = String::from_utf8(output.stdout).unwrap();
+    stdout = stdout.trim().to_string();
+
+    if stdout.len() == 0 {
+        out::print_error("Error: user.name was not found in git config.\n");
+        let name = ask_git_name();
+
+        let mut command = std::process::Command::new("git");
+        command.args(["config", "--global", "user.name", &name]);
+
+        let output = command.output().unwrap();
+
+        if !output.status.success() {
+            out::print_error("Error: Failed to set user.name.\n");
+            std::process::exit(1);
+        }
+    }
+
     let mut command = std::process::Command::new("git");
     command.args(["config", "--global", "user.email"]);
 
     let output = command.output().unwrap();
 
     if !output.status.success() {
+        out::print_error("Error: user.email was not found in git config.\n");
+        let email = ask_git_email();
+
+        let mut command = std::process::Command::new("git");
+        command.args(["config", "--global", "user.email", &email]);
+
+        let output = command.output().unwrap();
+
+        if !output.status.success() {
+            out::print_error("Error: Failed to set user.email.\n");
+            std::process::exit(1);
+        }
+    }
+
+    let mut stdout = String::from_utf8(output.stdout).unwrap();
+    stdout = stdout.trim().to_string();
+
+    if stdout.len() == 0 {
         out::print_error("Error: user.email was not found in git config.\n");
         let email = ask_git_email();
 
