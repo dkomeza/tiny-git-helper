b0b09148cb8169d10b44c01c54a10baab32b83ab
Dawid Komęza <dawid.komeza@gmail.com>
2023-12-10 17:27:33 +0100

    :rocket: Create install script for Linux & MacOS

---
+++ scripts/install.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+OS=""
+ARCH=$(uname -m)
+APP_NAME="tgh-$OS-$ARCH"
+REPO="dkomeza/tiny-git-helper"
+INSTALL_DIR="/usr/local/bin"
+
+get_latest_version() {
+    VERSION=$(curl -s https://api.github.com/repos/$REPO/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    if [ -z "$VERSION" ]; then
+        echo "Failed to retrieve the latest version."
+        exit 1
+    fi
+}
+
+check_supported_os() {
+    if [ "$(uname -s)" = "Linux" ]; then
+        OS="linux"
+    elif [ "$(uname -s)" = "Darwin" ]; then
+        OS="macos"
+    else
+        echo "Unsupported operating system: $(uname -s)"
+        exit 1
+    fi
+}
+
+check_supported_arch() {
+    if [ "$ARCH" != "x86_64" ] && [ "$ARCH" != "arm64" ]; then
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+    fi
+}
+
+download_and_install() {
+    DOWNLOAD_URL="https://github.com/$REPO/releases/download/$VERSION/$APP_NAME"
+    HTTP_STATUS_CODE=$(curl -s -L -o /dev/null -w "%{http_code}" "$DOWNLOAD_URL")
+    if [ "$HTTP_STATUS_CODE" -eq 200 ]; then
+        curl -L --output tgh "$DOWNLOAD_URL"
+        chmod +x tgh
+        mv tgh "$INSTALL_DIR"
+        echo "Installation complete."
+    else
+        echo "No suitable version found for your system."
+        exit 1
+    fi
+}
+
+check_supported_os
+check_supported_arch
+get_latest_version
+download_and_install
