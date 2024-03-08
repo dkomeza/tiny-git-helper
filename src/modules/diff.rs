9aca0a0442caacde99cff2936d72e08887f5a157
Dawid Komęza <dawid.komeza@gmail.com>
2024-03-08 19:29:56 +0100

    ✨ Add diff options

--- src/modules/diff.rs
+++ src/modules/diff.rs
@@ -6,4 +6,16 @@ mod views;
 pub use views::show_diff;
 
 #[derive(Parser)]
-pub struct DiffOptions {}
+pub struct DiffOptions {
+    /// Show the extended diff
+    #[clap(short, long)]
+    pub extended: bool,
+
+    /// The file to show the diff for
+    #[clap(short, long, conflicts_with = "selector")]
+    pub file: Option<String>,
+
+    /// Show the file selector
+    #[clap(short, long, conflicts_with = "file")]
+    pub selector: bool,
+}
