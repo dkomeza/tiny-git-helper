f0e20f939a2e379f0c77c54df8773c1ae29905a9
Dawid Komęza <dawid.komeza@gmail.com>
2024-11-09 11:58:33 +0100

    🔧 Update the install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,79 +1,243 @@
 #!/bin/sh
 
-OS=""
-ARCH=$(uname -m)
-APP_NAME="tgh-$OS-$ARCH-$VERSION"
-REPO="dkomeza/tiny-git-helper"
-INSTALL_DIR="/usr/local/bin"
+# Reset
+Color_Off=''
 
-echo "Installing Tiny Git Helper..."
+# Regular Colors
+Red=''
+Green=''
+Dim='' # White
 
-check_sudo() {
-    if [ "$(id -u)" -ne 0 ]; then
-        echo "Please run as root."
-        exit 1
-    fi
+# Bold
+Bold_White=''
+Bold_Green=''
+
+if [[ -t 1 ]]; then
+    # Reset
+    Color_Off='\033[0m' # Text Reset
+
+    # Regular Colors
+    Red='\033[0;31m'   # Red
+    Green='\033[0;32m' # Green
+    Dim='\033[0;2m'    # White
+
+    # Bold
+    Bold_Green='\033[1;32m' # Bold Green
+    Bold_White='\033[1m'    # Bold White
+fi
+
+error() {
+    echo "${Red}error${Color_Off}:" "$@" >&2
+    exit 1
 }
 
-get_latest_version() {
-    VERSION=$(curl -s https://api.github.com/repos/$REPO/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-    if [ -z "$VERSION" ]; then
-        echo "Failed to retrieve the latest version."
-        exit 1
-    fi
+info() {
+    echo "${Dim}$@ ${Color_Off}"
 }
 
-check_supported_os() {
-    if [ "$(uname -s)" = "Linux" ]; then
-        OS="linux"
-    elif [ "$(uname -s)" = "Darwin" ]; then
-        OS="macos"
+info_bold() {
+    echo "${Bold_White}$@ ${Color_Off}"
+}
+
+success() {
+    echo "${Green}$@ ${Color_Off}"
+}
+
+platform=$(uname -ms)
+
+if [[ ${OS:-} = Windows_NT ]]; then
+    echo "There is no install script for your platform: $platform"
+    exit $?
+fi
+
+case $platform in
+'Darwin x86_64')
+    target=darwin-x64
+    ;;
+'Darwin arm64')
+    target=darwin-arm64
+    ;;
+'Linux aarch64' | 'Linux arm64')
+    target=linux-arm64
+    ;;
+'Linux x86_64' | *)
+    target=linux-x64
+    ;;
+esac
+
+if [[ $target = darwin-x64 ]]; then
+    # Is this process running in Rosetta?
+    # redirect stderr to devnull to avoid error message when not running in Rosetta
+    if [[ $(sysctl -n sysctl.proc_translated 2>/dev/null) = 1 ]]; then
+        target=darwin-aarch64
+        info "Your shell is running in Rosetta 2. Downloading bun for $target instead"
+    fi
+fi
+
+install_env=TGH_INSTALL
+bin_env=\$$install_env/bin
+
+repo="https://github.com/dkomeza/tiny-git-helper"
+tgh_uri="$repo/releases/latest/download/tgh-$target.tar.gz"
+exe_name="tgh"
+
+install_dir=$HOME/.tgh
+bin_dir=$install_dir/bin
+exe=$bin_dir/$exe_name
+
+if [[ ! -d $bin_dir ]]; then
+    mkdir -p "$bin_dir"
+fi
+
+# Download the compressed tar file from github
+curl --fail --location --progress-bar --output "$exe" "$tgh_uri" ||
+    error "Failed to download tgh from \"$tgh_uri\""
+
+# Extract the tar file and remove it
+tar -xzf "$exe.tar.gz" -C "$install_dir" && rm -f "$exe.tar.gz"
+
+# Make the file executable
+chmod +x "$exe" || error "Failed to make $exe executable"
+
+tildify() {
+    if [[ $1 = $HOME/* ]]; then
+        local replacement=\~/
+
+        echo "${1/$HOME\//$replacement}"
     else
-        echo "Unsupported operating system: $(uname -s)"
-        exit 1
+        echo "$1"
     fi
 }
 
-check_supported_arch() {
-    if [ "$ARCH" = "aarch64" ]; then
-        ARCH="arm64"
-    fi
+success "tgh was installed successfully to $Bold_Green$(tildify "$exe")"
 
-    if [ "$ARCH" != "x86_64" ] && [ "$ARCH" != "arm64" ]; then
-        echo "Unsupported architecture: $ARCH"
-        exit 1
-    fi
-}
+refresh_command=''
+tilde_bin_dir=$(tildify "$bin_dir")
+quoted_install_dir=\"${install_dir//\"/\\\"}\"
 
-update_app_name() {
-    APP_NAME="tgh-$OS-$ARCH-$VERSION"
-}
+echo
 
-download_and_install() {
-    DOWNLOAD_URL="https://github.com/$REPO/releases/download/$VERSION/$APP_NAME"
-    HTTP_STATUS_CODE=$(curl -s -L -o /dev/null -w "%{http_code}" "$DOWNLOAD_URL")
+case $(basename "$SHELL") in
+fish)
+    commands=(
+        "set --export $install_env $quoted_install_dir"
+        "set --export PATH $bin_env \$PATH"
+    )
 
-    if [ "$HTTP_STATUS_CODE" -eq 200 ]; then
-        # Create temp directory
-        TMP_DIR=$(mktemp -d)
-        cd "$TMP_DIR" || exit 1
+    fish_config=$HOME/.config/fish/config.fish
+    tilde_fish_config=$(tildify "$fish_config")
 
-        curl -L -s --output tgh "$DOWNLOAD_URL"
-        chmod +x tgh
-        mv tgh "$INSTALL_DIR"
-        echo "Installation complete."
+    if [[ -w $fish_config ]]; then
+        {
+            echo '\n# tgh'
+
+            for command in "${commands[@]}"; do
+                echo "$command"
+            done
+        } >>"$fish_config"
+
+        info "Added \"$tilde_bin_dir\" to \$PATH in \"$tilde_fish_config\""
+
+        refresh_command="source $tilde_fish_config"
     else
-        echo "No suitable version found for your system."
-        echo "OS: $OS"
-        echo "ARCH: $ARCH"
-        echo "VERSION: $VERSION"
-        exit 1
-    fi
-}
+        echo "Manually add the directory to $tilde_fish_config (or similar):"
 
-check_sudo
-check_supported_os
-check_supported_arch
-get_latest_version
-update_app_name
-download_and_install
+        for command in "${commands[@]}"; do
+            info_bold "  $command"
+        done
+    fi
+    ;;
+zsh)
+    commands=(
+        "export $install_env=$quoted_install_dir"
+        "export PATH=\"$bin_env:\$PATH\""
+    )
+
+    zsh_config=$HOME/.zshrc
+    tilde_zsh_config=$(tildify "$zsh_config")
+
+    if [[ -w $zsh_config ]]; then
+        {
+            echo '\n# tgh'
+
+            for command in "${commands[@]}"; do
+                echo "$command"
+            done
+        } >>"$zsh_config"
+
+        info "Added \"$tilde_bin_dir\" to \$PATH in \"$tilde_zsh_config\""
+
+        refresh_command="exec $SHELL"
+    else
+        echo "Manually add the directory to $tilde_zsh_config (or similar):"
+
+        for command in "${commands[@]}"; do
+            info_bold "  $command"
+        done
+    fi
+    ;;
+bash)
+    commands=(
+        "export $install_env=$quoted_install_dir"
+        "export PATH=$bin_env:\$PATH"
+    )
+
+    bash_configs=(
+        "$HOME/.bashrc"
+        "$HOME/.bash_profile"
+    )
+
+    if [[ ${XDG_CONFIG_HOME:-} ]]; then
+        bash_configs+=(
+            "$XDG_CONFIG_HOME/.bash_profile"
+            "$XDG_CONFIG_HOME/.bashrc"
+            "$XDG_CONFIG_HOME/bash_profile"
+            "$XDG_CONFIG_HOME/bashrc"
+        )
+    fi
+
+    set_manually=true
+    for bash_config in "${bash_configs[@]}"; do
+        tilde_bash_config=$(tildify "$bash_config")
+
+        if [[ -w $bash_config ]]; then
+            {
+                echo '\n# tgh'
+
+                for command in "${commands[@]}"; do
+                    echo "$command"
+                done
+            } >>"$bash_config"
+
+            info "Added \"$tilde_bin_dir\" to \$PATH in \"$tilde_bash_config\""
+
+            refresh_command="source $bash_config"
+            set_manually=false
+            break
+        fi
+    done
+
+    if [[ $set_manually = true ]]; then
+        echo "Manually add the directory to $tilde_bash_config (or similar):"
+
+        for command in "${commands[@]}"; do
+            info_bold "  $command"
+        done
+    fi
+    ;;
+*)
+    echo 'Manually add the directory to ~/.bashrc (or similar):'
+    info_bold "  export $install_env=$quoted_install_dir"
+    info_bold "  export PATH=\"$bin_env:\$PATH\""
+    ;;
+esac
+
+echo
+info "To get started, run:"
+echo
+
+if [[ $refresh_command ]]; then
+    info_bold "  $refresh_command"
+fi
+
+info_bold "  tgh --help"
