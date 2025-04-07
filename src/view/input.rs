7a43cc1929fc329041edc78b4d4b42414bfbbfab
Dawid Komęza <dawid.komeza@gmail.com>
2025-04-07 20:32:55 +0200

    ✨ Initial list input implementation

--- src/view/input.rs
+++ src/view/input.rs
@@ -1,14 +1,17 @@
 use crossterm::{
-    cursor::MoveToColumn,
+    cursor::{MoveDown, MoveToColumn, MoveUp},
     event::{self, KeyCode, KeyModifiers},
     execute,
     terminal::{self, disable_raw_mode, enable_raw_mode, ClearType},
 };
-use std::io::{self, Write};
+use std::{
+    fmt::Display,
+    io::{self, Write},
+};
 
-use crate::view::print;
+use super::{print, PrintSize};
 
-use super::PrintSize;
+const MAX_ROWS: usize = 12;
 
 pub enum ReturnType {
     Cancel,
@@ -28,13 +31,154 @@ pub fn password(prompt: &str) -> Result<String, ReturnType> {
     get_user_text_input(prompt, TextInputType::Password)
 }
 
+pub fn list<T>(prompt: &str, items: Vec<T>) -> Result<T, ReturnType>
+where
+    T: Display + Clone,
+{
+    enable_raw_mode().unwrap();
+    let mut selected = 0;
+    let mut offset = 0;
+
+    let PrintSize {
+        cols: prompt_length,
+        rows: _,
+    } = print(format!("{}\n", prompt));
+
+    let total_rows = items.len();
+    let available_rows = terminal::size().unwrap().1 as usize - 1; // 1 for the prompt
+
+    let mut usable_rows = if total_rows > available_rows {
+        available_rows
+    } else {
+        total_rows
+    };
+    if usable_rows > MAX_ROWS {
+        usable_rows = MAX_ROWS;
+    }
+
+    for i in offset..usable_rows + offset {
+        if i == selected {
+            print(format!("$cc `>` {}\n", items[i]));
+        } else {
+            if i == usable_rows + offset - 1 && total_rows > usable_rows {
+                print(format!("▼ {}\n", items[i]));
+            } else {
+                print(format!("  {}\n", items[i]));
+            }
+        }
+    }
+
+    execute!(
+        io::stdout(),
+        MoveUp((usable_rows + 1) as u16),
+        MoveToColumn((prompt_length) as u16)
+    )
+    .unwrap();
+
+    loop {
+        if let Ok(event) = event::read() {
+            match event {
+                event::Event::Key(event) => match event.code {
+                    KeyCode::Esc => {
+                        execute!(
+                            io::stdout(),
+                            MoveToColumn(0),
+                            terminal::Clear(ClearType::CurrentLine)
+                        )
+                        .unwrap();
+                        print(format!("{}$cr `canceled`\n", prompt));
+
+                        disable_raw_mode().unwrap();
+                        return Err(ReturnType::Cancel);
+                    }
+                    KeyCode::Enter => {
+                        break;
+                    }
+                    KeyCode::Up => {
+                        if selected > 0 {
+                            selected -= 1;
+                        }
+                    }
+                    KeyCode::Down => {
+                        if selected < total_rows - 1 {
+                            selected += 1;
+                        }
+                    }
+                    KeyCode::Char('c') => {
+                        if event.modifiers == KeyModifiers::CONTROL {
+                            write!(io::stdout(), "\n\r").unwrap();
+                            disable_raw_mode().unwrap();
+                            return Err(ReturnType::Exit);
+                        }
+                    }
+                    _ => {}
+                },
+                _ => {}
+            }
+
+            let diff = (selected + 1) as isize - (usable_rows / 2) as isize;
+            if diff <= 0 {
+                offset = 0;
+            } else if diff > 0 {
+                offset = diff as usize;
+            }
+            if offset > total_rows - usable_rows {
+                offset = total_rows - usable_rows;
+            }
+
+            // Render the prompt
+            execute!(
+                io::stdout(),
+                MoveToColumn(0),
+                terminal::Clear(ClearType::FromCursorDown)
+            )
+            .unwrap();
+            print(format!("{} {}", prompt, diff));
+            execute!(io::stdout(), MoveDown(1), MoveToColumn(0)).unwrap();
+
+            // Render the list
+            for i in offset..usable_rows + offset {
+                if i == selected {
+                    print(format!("$cc `>` {}", items[i]));
+                } else {
+                    if i == usable_rows + offset - 1 && total_rows > usable_rows + offset {
+                        print(format!("▼ {}", items[i]));
+                    } else if offset > 0 && i == offset {
+                        print(format!("▲ {}", items[i]));
+                    } else {
+                        print(format!("  {}", items[i]));
+                    }
+                }
+                execute!(io::stdout(), MoveDown(1), MoveToColumn(0)).unwrap();
+            }
+
+            // Go back to the prompt
+            execute!(
+                io::stdout(),
+                MoveUp((usable_rows + 1) as u16),
+                MoveToColumn((prompt_length) as u16)
+            )
+            .unwrap();
+
+            io::stdout().flush().unwrap();
+        }
+    }
+
+    disable_raw_mode().unwrap();
+
+    Ok(items[selected].clone())
+}
+
 fn get_user_text_input(prompt: &str, input_type: TextInputType) -> Result<String, ReturnType> {
     enable_raw_mode().unwrap();
 
     let mut input = String::new();
     let mut cursor_position = 0;
 
-    let PrintSize{cols: prompt_length, rows: _} = print(format!("{}", prompt));
+    let PrintSize {
+        cols: prompt_length,
+        rows: _,
+    } = print(format!("{}", prompt));
     io::stdout().flush().unwrap();
 
     loop {
